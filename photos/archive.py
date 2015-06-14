dfd4fdb9db8f3cd3b69ec05510b8201a0a72950f
Juti Noppornpitak <jnopporn@shiroyuki.com>
2015-06-13 20:34:21 -0400

    Renamed the submodule for active model

--- photos/archive.py
+++ photos/archive.py
@@ -1,4 +1,4 @@
-from photos.model import Photograph as P
+from photos.active_model import Photograph as P
 
 class Archive(object):
     def open(self, location):
--- photos/command.py
+++ photos/command.py
@@ -1,5 +1,29 @@
 from photos.prototype import ICommand
 
+class MetadataAggregator(ICommand):
+    """ Inspect the photo metadata. """
+
+    def __init__(self, archive):
+        self.archive = archive
+
+    def define(self, parser):
+        parser.add_argument(
+            '--debug',
+            help='Enable the debugging mode.'
+        )
+
+    def execute(self, args):
+        photo = self.archive.open(args.path)
+
+        print('Location:  {}'.format(photo.location))
+        print('SHA1 Hash: {}'.format(photo.hashsum))
+
+        info = photo.info()
+
+        for l in info:
+            v = info[l]
+            print('{:<20}: {}'.format(l, v))
+
 class Inspector(ICommand):
     """ Inspect the photo metadata. """
 
